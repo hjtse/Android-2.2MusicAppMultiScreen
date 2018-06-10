dde3e1a0e0b2f8379f46d7d4abaadb26ad8e33ed
hjtse <hanjohntse@gmail.com>
2018-06-10 14:53:10 -0400

    first commit. create repo

---
+++ app/src/main/java/com/example/android/music/MainActivity.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.music;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the numbers category
+        final TextView numbers = (TextView) findViewById(R.id.allsongs);
+
+        // Set a click listener on that View
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link AllSongs}
+                Intent allSongsIntent = new Intent(MainActivity.this, AllSongs.class);
+
+                // Start the new activity
+                startActivity(allSongsIntent);
+            }
+        });
+
+
+        // Find the View that shows the family category
+        final TextView family = (TextView) findViewById(R.id.favorites);
+        // Set a click listener on that View
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link Favorites}
+                Intent favoritesIntent = new Intent(MainActivity.this, Favorites.class);
+
+                // Start the new activity
+                startActivity(favoritesIntent);
+            }
+        });
+
+
+        // Find the View that shows the colors category
+        final TextView colors = (TextView) findViewById(R.id.artists);
+
+        // Set a click listener on that View
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link Artists}
+                Intent artistsIntent = new Intent(MainActivity.this, Artists.class);
+
+                // Start the new activity
+                startActivity(artistsIntent);
+            }
+        });
+
+
+        // Find the View that shows the phrases category
+        final TextView phrases = (TextView) findViewById(R.id.albums);
+
+        // Set a click listener on that View
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link Albums}
+                Intent albumsIntent = new Intent(MainActivity.this, Albums.class);
+
+                //Start the new activity
+                startActivity(albumsIntent);
+            }
+        });
+
+
+    }
+
+
+}
+
+
